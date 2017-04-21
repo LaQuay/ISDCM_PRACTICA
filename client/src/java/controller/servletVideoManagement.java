a93b59702c719b49d4a3ec782da912b4b14762bf
Marc Vila <marcvilagomez@gmail.com>
2017-04-21 12:50:08 +0200

    Sacando imports no utilizados

--- client/src/java/controller/servletVideoManagement.java
+++ client/src/java/controller/servletVideoManagement.java
@@ -16,7 +16,6 @@ import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
-import model.Usuario;
 import model.Video;
 
 //Si falla glassfish: 
