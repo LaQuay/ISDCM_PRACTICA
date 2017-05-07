d99000eda2afe0f513b3c4912f760e44c043d90b
Marc Vila <marcvilagomez@gmail.com>
2017-05-07 19:36:23 +0200

    Servicio REST implementado

---
+++ server/REST/src/java/rest/GenericResource.java
@@ -0,0 +1,55 @@
+package rest;
+
+import javax.ws.rs.core.Context;
+import javax.ws.rs.core.UriInfo;
+import javax.ws.rs.Produces;
+import javax.ws.rs.Consumes;
+import javax.ws.rs.FormParam;
+import javax.ws.rs.GET;
+import javax.ws.rs.Path;
+import javax.ws.rs.POST;
+import javax.ws.rs.QueryParam;
+
+/**
+ * REST Web Service
+ *
+ * @author marc.vila.gomez
+ */
+@Path("generic")
+public class GenericResource {
+
+    @Context
+    private UriInfo context;
+
+    /**
+     * Creates a new instance of GenericResource
+     */
+    public GenericResource() {
+    }
+
+    /**
+     * Sample of GET method
+     * @param idPelicula
+     * @return 
+     */
+    @Path("getInfo")
+    @GET    
+    @Produces("text/html")
+    public String getInfo(@QueryParam("idPelicula") String idPelicula) {
+        return "<html><head></head> <body> Actualizando pelicula con ID: " + idPelicula + " reproducciones actuales: " + 0;
+    }
+
+    /**
+     * Sample of POST method
+     * 
+     * @param idPelicula
+     * @return 
+     */
+    @Path("postinfo")   
+    @POST    
+    @Consumes("application/x-www-form-urlencoded")
+    @Produces("text/html")
+    public String postInfo(@FormParam("idPelicula") String idPelicula){                
+        return "<html><head></head> <body> Actualizando pelicula con ID: " + idPelicula + " reproducciones actuales: " + 0;
+    }    
+}
